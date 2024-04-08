71ae725c272d4f44787ac1156442c2134cf60c9d
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-04-08 22:25:26 +0300

    Code refactoring: change logic for added tasks and todolists

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
 import {FilterValuesType, TaskType} from "./App";
 import {ChangeEvent, KeyboardEvent, useState} from "react";
 import {Button} from "./Button";
+import {AddItemForm} from "./AddItemForm";
 
 type PropsType = {
 	title: string
@@ -27,31 +28,12 @@ export const Todolist = (props: PropsType) => {
 		removeTodolist
 	} = props
 
-	const [taskTitle, setTaskTitle] = useState('')
-	const [error, setError] = useState<string | null>(null)
-
-	const addTaskHandler = () => {
-		if (taskTitle.trim() !== '') {
-			addTask(taskTitle.trim(), todolistId)
-			setTaskTitle('')
-		} else {
-			setError('Title is required')
-		}
-	}
-
-	const changeTaskTitleHandler = (event: ChangeEvent<HTMLInputElement>) => {
-		setTaskTitle(event.currentTarget.value)
-	}
-
-	const addTaskOnKeyUpHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-		setError(null)
-		if (event.key === 'Enter') {
-			addTaskHandler()
-		}
-	}
-
 	const changeFilterTasksHandler = (filter: FilterValuesType) => {
 		changeFilter(filter, props.todolistId)
+	}
+
+	const addTaskCallback = (title:string) => {
+		addTask(title, todolistId)
 	}
 
 	const removeTodolistHandler = () => {
@@ -64,16 +46,7 @@ export const Todolist = (props: PropsType) => {
 				<h3>{title}</h3>
 				<Button title={'x'} onClick={removeTodolistHandler}/>
 			</div>
-			<div>
-				<input
-					className={error ? 'error' : ''}
-					value={taskTitle}
-					onChange={changeTaskTitleHandler}
-					onKeyUp={addTaskOnKeyUpHandler}
-				/>
-				<Button title={'+'} onClick={addTaskHandler}/>
-				{error && <div className={'error-message'}>{error}</div>}
-			</div>
+			<AddItemForm addItem={addTaskCallback}/>
 			{
 				tasks.length === 0
 					? <p>Тасок нет</p>
