0f4911ca22411c89dfc1c2e5965b784cb5d8bf66
Kirill190611 <k.igorevitch88@yandex.ru>
2024-08-18 08:22:29 +0300

    Return project to init state for practice with RTK

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,69 +1,64 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { authAPI, LoginParamsType } from 'api/todolists-api'
-import { AppThunk } from 'app/store'
-import {} from 'app/app-reducer'
-import {
-  handleServerAppError,
-  handleServerNetworkError,
-} from 'utils/error-utils'
-import { setAppStatusAC } from 'app/_app-reducer'
-
-type InitialStateType = {
-  isLoggedIn: boolean
-}
+import {Dispatch} from 'redux'
+import {SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../../app/app-reducer'
+import {authAPI, LoginParamsType} from '../../api/todolists-api'
+import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
 
 const initialState: InitialStateType = {
-  isLoggedIn: false,
+    isLoggedIn: false
 }
 
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setIsLoggedInAC(state, action: PayloadAction<boolean>) {
-      return { ...state, isLoggedIn: action.payload }
-    },
-  },
-})
+export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return {...state, isLoggedIn: action.value}
+        default:
+            return state
+    }
+}
 
-export const loginTC =
-  (data: LoginParamsType): AppThunk =>
-  (dispatch) => {
+// actions
+
+export const setIsLoggedInAC = (value: boolean) =>
+    ({type: 'login/SET-IS-LOGGED-IN', value} as const)
+
+
+// thunks
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
     dispatch(setAppStatusAC('loading'))
-    authAPI
-      .login(data)
-      .then((res) => {
-        if (res.data.resultCode === 0) {
-          dispatch(setIsLoggedInAC(true))
-          dispatch(setAppStatusAC('succeeded'))
-        } else {
-          handleServerAppError(res.data, dispatch)
-        }
-      })
-      .catch((error) => {
-        handleServerNetworkError(error, dispatch)
-      })
-  }
-
-export const logoutTC = (): AppThunk => (dispatch) => {
-  dispatch(setAppStatusAC('loading'))
-  authAPI
-    .logout()
-    .then((res) => {
-      if (res.data.resultCode === 0) {
-        dispatch(setIsLoggedInAC(false))
-        dispatch(setAppStatusAC('succeeded'))
-      } else {
-        handleServerAppError(res.data, dispatch)
-      }
-    })
-    .catch((error) => {
-      handleServerNetworkError(error, dispatch)
-    })
+    authAPI.login(data)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(true))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+export const logoutTC = () => (dispatch: Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>) => {
+    dispatch(setAppStatusAC('loading'))
+    authAPI.logout()
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                dispatch(setIsLoggedInAC(false))
+                dispatch(setAppStatusAC('succeeded'))
+            } else {
+                handleServerAppError(res.data, dispatch)
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
 }
 
-export const authReducer = authSlice.reducer
-export const { setIsLoggedInAC } = authSlice.actions
+// types
 
-//Todo: rename type to remove word 'type' and rename from init to authInit
-//Todo: rename actions creator to remove AC
+type ActionsType = ReturnType<typeof setIsLoggedInAC>
+type InitialStateType = {
+    isLoggedIn: boolean
+}
+
+type ThunkDispatch = Dispatch<ActionsType | SetAppStatusActionType | SetAppErrorActionType>
