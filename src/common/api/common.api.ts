2f5c2098895afb1e10fc9393faec071520f0f262
Kirill190611 <k.igorevitch88@yandex.ru>
2024-08-30 21:53:02 +0300

    return to basic state spr05w03

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   withCredentials: true,
   headers: {
-    "API-KEY": "1cdd9f77-c60e-4af5-b194-659e4ebd5d41",
+    'API-KEY': '25fabb7c-9b99-4c08-bf07-cc05cdafd446',
   },
-});
+})
