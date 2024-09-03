256a490046b966d72d363b6e45a838cff7529522
Kirill190611 <k.igorevitch88@yandex.ru>
2024-09-03 20:30:02 +0300

    Added JSDoc for handle-server-app-error function

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -2,11 +2,21 @@ import { Dispatch } from 'redux'
 import { appActions } from 'app/app.reducer'
 import { BaseResponse } from 'common/types/common.types'
 
+/**
+ * Обрабатывает ошибки сервера и обновляет состояние приложения.
+ *
+ * @template D - Тип данных, ожидаемых в ответе сервера.
+ * @param data - Ответ от сервера, содержащий данные и сообщения об ошибках.
+ * @param dispatch - функция для отправки действий (actions) в Redux хранилище.
+ * @param isShowGlobalError - Флаг, указывающий, нужно ли показывать глобальную ошибку.
+ * @returns nothing return
+ */
+
 export const handleServerAppError = <D>(
   data: BaseResponse<D>,
   dispatch: Dispatch,
   isShowGlobalError: boolean = true
-) => {
+): void => {
   if (isShowGlobalError) {
     const error = data.messages.length
       ? data.messages[0]
