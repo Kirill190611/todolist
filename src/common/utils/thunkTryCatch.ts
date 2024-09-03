3ab6e2c1e9700f886e7cd54c3291a1f62d04a1e9
Kirill190611 <k.igorevitch88@yandex.ru>
2024-09-03 22:06:26 +0300

    add type fot thunkTryCatch function

--- src/common/utils/thunkTryCatch.ts
+++ src/common/utils/thunkTryCatch.ts
@@ -1,16 +1,20 @@
 import { appActions } from 'app/app.reducer'
 import { handleServerNetworkError } from 'common/utils/handle-server-network-error'
-import { AppDispatch } from 'app/store'
+import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk'
+import { AppDispatch, AppRootStateType } from 'app/store'
+import { BaseResponse } from 'common/types/common.types'
 
-type ThunkApi = {
-  dispatch: AppDispatch
-  rejectWithValue: any
-}
+type ThunkApi = BaseThunkAPI<
+  AppRootStateType,
+  unknown,
+  AppDispatch,
+  null | BaseResponse
+>
 
-export const thunkTryCatch = async (
+export const thunkTryCatch = async <T>(
   thunkAPI: ThunkApi,
-  logic: () => Promise<any>
-) => {
+  logic: () => Promise<T>
+): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
   const { dispatch, rejectWithValue } = thunkAPI
   dispatch(appActions.setAppStatus({ status: 'loading' }))
   try {
